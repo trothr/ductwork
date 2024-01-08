b0755ad21ff31cf54fbe9c4059534d03a1acbb72
Rick Troth <trothr@gmail.com>
2024-01-07 21:44:24 -0500

    XFL prefix

--- src/stages/console.c
+++ src/stages/console.c
@@ -2,15 +2,14 @@
  *
  *        Name: console.c (C program source)
  *              Ductwork CONSOLE stage
- *        Date: 2010-Mar-24
- *              2023-07
+ *        Date: 2010-Mar-24, 2023-07
  *
  */
 
 #include <stdio.h>
 
 
-#include <xpllib.h>
+#include <xfl.h>
 
 /* ------------------------------------------------------------------ */
 int main(int argc,char*argv[])
@@ -27,14 +26,14 @@ printf("console: (starting)\n");
      * to provide all arguments as a single string in argv[1].        */
 
     /* initialize this stage                                          */
-    rc = xplstagestart(&pc);
+    rc = xfl_stagestart(&pc);
     if (rc < 0) return 1;
 
     /* snag the first input stream and the first output stream        */
     pi = po = NULL;
     for (pn = pc; pn != NULL; pn = pn->next)
-      { if (pn->flag & XPL_OUTPUT) { if (po == NULL) po = pn; }
-        if (pn->flag & XPL_INPUT)  { if (pi == NULL) pi = pn; } }
+      { if (pn->flag & XFL_OUTPUT) { if (po == NULL) po = pn; }
+        if (pn->flag & XFL_INPUT)  { if (pi == NULL) pi = pn; } }
 
     /* if pi is null then we are a first stage so slurp stdin         */
     if (pi == NULL && po == NULL) return 1;
@@ -43,20 +42,20 @@ printf("console: (starting)\n");
     while (1) {
 printf("console: (top of loop)\n");
         buflen = sizeof(buffer) - 1;
-        rc = xplpeekto(pi,buffer,buflen);             /* sip on input */
+        rc = xfl_peekto(pi,buffer,buflen);             /* sip on input */
         if (rc < 0) break; /* else */ buflen = rc;
 printf("console: '%s' %d (input peeked)\n",buffer,rc);
 printf("\n%s\n\n",buffer);
         if (po != NULL)
-        rc = xploutput(po,buffer,buflen);       /* send it downstream */
+        rc = xfl_output(po,buffer,buflen);       /* send it downstream */
         if (rc < 0) break;
-        xplreadto(pi,NULL,0);     /* consume the record after sending */
+        xfl_readto(pi,NULL,0);     /* consume the record after sending */
 printf("console: (record consumed)\n");
       }
     if (rc < 0) return 1;
 
     /* terminate this stage cleanly                                   */
-    rc = xplstagequit(pc);
+    rc = xfl_stagequit(pc);
     if (rc < 0) return 1;
 
 printf("console: (normal exit)\n");
@@ -91,7 +90,6 @@ int oldmain()
 
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 #endif
 
---
+++ src/xfl.h
@@ -0,0 +1,81 @@
+/*
+ *
+ *        Name: xfl.h (C program header)
+ *        Date: 2023-06-12 (Mon) roughly, 2024-01-07 (Sun)
+ *
+ *
+
+**
+
+From: Nancy Foley <nfoley@us.ibm.com>
+To: "rmt@casita.net" <rmt@casita.net>
+Subject: Prefix Request
+Date: Wed, 26 Jul 2023 14:37:20 +0000
+
+Prefix XFL* has been assigned to you and your product.
+If/when you have a product number assigned, please let me know
+so that I may update the prefix database accordingly. Thanks.
+
+Nancy J. Foley
+element@us.ibm.com
+IBM C-S 0-2043-003
+zStack Custom Build, Software Operations
+IBM Systems
+
+ */
+
+#ifndef _XFLLIB_H
+
+//static char *_ductwork_version = "Ductwork 0.7.5";
+#define  XFL_VERSION  (((0) << 24) + ((7) << 16) + ((5) << 8) + (0))
+//static int xfl_version = XFL_VERSION;
+
+#define     XFL_INPUT   0x0001
+#define     XFL_OUTPUT  0x0002
+
+typedef struct PIPECONN {
+
+    int fdf;         /* forward flowing file descriptor used for data */
+                 /* data flows "downstream" from producer to consumer */
+    int fdr;      /* reverse flowing file descriptor used for control */
+                 /* control goes "upstream" from consumer to producer */
+    int flag;   /* which side of the connection, producer or consumer */
+
+    char name[16];            /* name of connector for a named stream */
+    int n;               /* number of connector for a numbered stream */
+
+    int rn;   /* record number - how many records have been processed */
+    /* This leads to 1-based indexing because when this is zero       */
+    /* nothing has happened yet. So there is no "record zero".        */
+
+    void *buff;        /* optional buffer for shared memory transfers */
+    void *glob;                /* global area */
+    void *prev;                /* pointer to previous struct in chain */
+    void *next;                /* pointer to next struct in the chain */
+
+                        } PIPECONN;
+
+
+char*xfl_argcat(int,char*[]);                   /* FKA xplcatargs     */
+
+
+int xfl_parse(char*,int*,char*[],char*);
+int xfl_error(int,int,char**,char*);
+
+
+int xfl_stagestart(PIPECONN**);
+
+int xfl_peekto(PIPECONN*,void*,int);      /* pipeconn, buffer, buflen */
+int xfl_readto(PIPECONN*,void*,int);      /* pipeconn, buffer, buflen */
+int xfl_output(PIPECONN*,void*,int);      /* pipeconn, buffer, buflen */
+
+int xfl_stagequit(PIPECONN*);
+
+
+int xplstagespawn(int,char**,PIPECONN*,PIPECONN*);
+
+
+#define _XFLLIB_H
+#endif
+
+
